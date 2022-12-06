07d1c7c02f51eeaca4941b692369c8080326957d
MMSuperD <shdjxywd@sina.com>
2022-12-06 22:58:11 +0800

    feat(C++):初始化项目

---
+++ C++StudyLibrary/C++StudyLibrary/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C++StudyLibrary
+//
+//  Created by 王丹 on 2022/12/6.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
